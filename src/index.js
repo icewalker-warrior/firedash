4ffd9af0fd49461a1ae1003bb7bc869c5ac5e5a0
miguel <miguel@MacBook-Air-de-miguel.local>
2019-07-05 17:44:15 -0300

    init project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.scss";
+ReactDOM.render(
+  <div>
+    <h1>FireDashboard</h1>
+  </div>
+  , document.getElementById('root')
+);
